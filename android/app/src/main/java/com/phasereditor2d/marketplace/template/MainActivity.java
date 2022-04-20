7b7db1e7187316397d401b3a318df1df309f8d62
PhaserEditor2D <developers@phasereditor2d.com>
2022-04-20 11:44:40 -0400

    Adds capacitor and android.

---
+++ android/app/src/main/java/com/phasereditor2d/marketplace/template/MainActivity.java
@@ -0,0 +1,5 @@
+package com.phasereditor2d.marketplace.template;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.phasereditor2d.marketplace.template',
+  appName: 'phasereditor2d-capacitorjs-template',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
